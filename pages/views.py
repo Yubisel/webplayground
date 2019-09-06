36a87efe47f4dae1353d4fd91c71a91122dd6052
Yubisel <yubiselv@gmail.com>
2019-09-06 13:24:45 -0400

    terminado el mixin de identificacion en pages

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,48 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from .models import Page
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
+
+from .forms import PageForm
+
+class StaffRequiredMixin(object):
+    '''
+    Este mixin requerira que el usuario sea miembro del staff
+    '''
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        #if not request.user.is_staff:
+        #    return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
 
 # Create your views here.
-def pages(request):
-    pages = get_list_or_404(Page)
-    return render(request, 'pages/pages.html', {'pages':pages})
+class PageListView(ListView):
+    model = Page
 
-def page(request, page_id, page_slug):
-    page = get_object_or_404(Page, id=page_id)
-    return render(request, 'pages/page.html', {'page':page})
+class PageDetailView(DetailView):
+    model = Page
+
+@method_decorator(staff_member_required, name='dispatch')
+class PageCreate(CreateView):
+    model = Page
+    form_class = PageForm
+    #fields = ['title', 'content', 'order']
+    success_url = reverse_lazy('pages:pages')
+
+class PageUpdate(StaffRequiredMixin, UpdateView):
+    model = Page
+    form_class = PageForm
+    #fields = ['title', 'content', 'order']
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('pages:pages')
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+class PageDelete(StaffRequiredMixin, DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
--- webplayground/urls.py
+++ webplayground/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from pages.urls import page_patterns
+
 urlpatterns = [
     path('', include('core.urls')),
+    path('pages/', include(page_patterns)),
     path('admin/', admin.site.urls),
 ]
